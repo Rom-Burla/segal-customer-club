191d8020906e905c542131b6667d841f159b36d7
Rom Burla <romwebdev@gmail.com>
2022-11-21 05:31:41 +0200

    debugs and fixes

--- public/app/customer.js
+++ public/app/customer.js
@@ -53,6 +53,7 @@ function childAgeValidationN(evt, input, inputLabel) {
         evt.preventDefault();
     }
     else if (parseInt(input.value) > 14) {
+        evt.preventDefault();
         alert('גיל הילד חייב להיות קטן מ14');
     }
     else {
