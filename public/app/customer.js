e9037bb213e85f9a293b03ebfbfde76bddb97589
Rom Burla <romwebdev@gmail.com>
2022-11-21 09:42:42 +0200

    debug

--- public/app/customer.js
+++ public/app/customer.js
@@ -6,25 +6,25 @@ let childAgeNo = document.getElementById('childAgeNo');
 let userId = document.getElementById('userId');
 // end of child registration no children inputs
 // regex
-const hebNameRegexN = /[\u0590-\u05FF\u200f\u200e]{2,9}[-\s]{0,1}[\u0590-\u05FF\u200f\u200e]{0,9}[-\s]{0,1}[\u0590-\u05FF\u200f\u200e]{0,9}/i;
+const hebNameRegexN = /[\u0590-\u05FF\u200f\u200e]{2,9}[-\s]{0,1}[\u0590-\u05FF\u200f\u200e]{0,9}[-\s]{0,1}[\u0590-\u05FF\u200f\u200e]{0,9}/iu;
 const ageRegexN = new RegExp('^[0-9]{1,2}$');
 // end of regex
 // form validation
 childRegNo.addEventListener('submit', (evt) => {
     if (userId.value.length === 0) {
-        evt.preventDefault();
         alert('המשתמש לא רשום');
+        evt.preventDefault();
     }
     else {
         if (childNameNo.value.length === 0) {
-            evt.preventDefault();
             alert('השדה שם הילד ריק והוא שדה חובה');
+            evt.preventDefault();
         }
         else {
             hebNameValidationN(evt, childNameNo, 'שם הילד');
             if (childAgeNo.value.length === 0) {
-                evt.preventDefault();
                 alert('השדה גיל הילד ריק והוא שדה חובה');
+                evt.preventDefault();
             }
             else {
                 childAgeValidationN(evt, childAgeNo, 'גיל הילד');
